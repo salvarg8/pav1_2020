2b482947fcc6a16948bb94a61cdeafd8f124d8be
Salvador Rodriguez Garraza <salvarodriguez_91@hotmail.com>
2020-09-11 12:30:21 -0300

    reorganice formularios

--- TpiBugs/TpiBugs/Program.cs
+++ TpiBugs/TpiBugs/Program.cs
@@ -16,7 +16,7 @@ namespace TpiBugs
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmLogin());
         }
     }
 }
